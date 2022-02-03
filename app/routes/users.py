77d926b8f06ef2ba9080844eb56cf303320dd4cd
Madhu Prakash <beharamadhu270405@gmail.com>
2022-02-03 22:53:13 +0530

    Adding basic templates and api setup

---
+++ app/routes/users.py
@@ -0,0 +1,23 @@
+from fastapi import APIRouter, Depends
+
+router = APIRouter(
+    prefix="/users",
+    tags=["users"],
+    # dependencies=[Depends(get_token_header)],
+    responses={404: {"description": "Not found"}},
+)
+
+
+@router.get("/users/", tags=["users"])
+async def read_users():
+    return [{"username": "Rick"}, {"username": "Morty"}]
+
+
+@router.get("/users/me", tags=["users"])
+async def read_user_me():
+    return {"username": "fakecurrentuser"}
+
+
+@router.get("/users/{username}", tags=["users"])
+async def read_user(username: str):
+    return {"username": username}
--- run.py
+++ run.py
@@ -1,11 +1,20 @@
 
 import uvicorn
 from fastapi import Depends, FastAPI, HTTPException
+from app.routes import users
 
 app = FastAPI(
     title='FastAPI JWT', openapi_url='/openapi.json', docs_url='/docs',
     description='fastapi jwt')
 
+
+app.include_router(users.router)
+
+
+
+@app.get("/")
+async def root():
+    return {"message": "Hello Bigger Applications!"}
 
 if __name__ == "__main__":
     uvicorn.run("run:app", host="127.0.0.1", port=5000, log_level="info")
